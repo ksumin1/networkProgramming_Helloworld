d7b8af1baccf6757df8378d015303605b29ac868
user <w2005@e-mirim.hs.kr>
2022-03-21 13:51:54 +0900

    urls, playground/urls: playground/ -> hello/ -> say_hello()

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import playground.views
 
 urlpatterns = [
-    path('playground/hello/', playground.views.say_hello),
+    path('playground/', include('playground.urls')),
     path('admin/', admin.site.urls),
 ]
